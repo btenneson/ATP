7520adad6d99ab84274cdeaf3fd51df9b614217d
btenneson <btenneson2301@baypath.edu>
2026-09-03 22:35:17 -0700

    DATA MIND 3.2: export oracle ATP dynamics interfaces

--- data_mind_3_2/epistemic/__init__.py
+++ data_mind_3_2/epistemic/__init__.py
@@ -1,16 +1,40 @@
-"""Epistemic horizon and oracle interfaces for DATA MIND 3.2."""
+"""Epistemic horizon, oracle, and factorized ATP interfaces for DATA MIND 3.2."""
 
 from .horizon import FiniteHorizonReport, finite_horizon_report
 from .oracle import FiniteHorizonOracle, OracleHint, OracleRecord, OracleUseMode
 from .integration import AwarenessCue, OracleAwarenessBridge
+from .oracle_dynamics import (
+    ALL_ORACLE_FACETS,
+    AbelObservation,
+    FactorizedOracleATP,
+    OracleATPState,
+    OracleFacet,
+    OraclePartition,
+    OracleTransformation,
+    TransitionRecord,
+    all_oracle_partitions,
+    mean_abel_increment,
+    mean_abel_residual,
+)
 
 __all__ = [
+    "ALL_ORACLE_FACETS",
+    "AbelObservation",
     "AwarenessCue",
+    "FactorizedOracleATP",
     "FiniteHorizonOracle",
     "FiniteHorizonReport",
+    "OracleATPState",
     "OracleAwarenessBridge",
+    "OracleFacet",
     "OracleHint",
+    "OraclePartition",
     "OracleRecord",
+    "OracleTransformation",
     "OracleUseMode",
+    "TransitionRecord",
+    "all_oracle_partitions",
     "finite_horizon_report",
+    "mean_abel_increment",
+    "mean_abel_residual",
 ]
